8b4bfaa40055e4e6733b516a69d607d6850b6a0f
Brandon Cook <phylake@gmail.com>
2014-01-18 20:14:25 -0800

    moved from avm3

---
+++ Tests.hs
@@ -0,0 +1,23 @@
+module Tests (tests) where
+
+import           Test.QuickCheck
+import           Distribution.TestSuite
+
+tests :: IO [Test]
+--tests = return []
+tests = return [ {-Test succeeds,-} Test fails ]
+  where
+    succeeds = TestInstance
+        { run = return $ Finished Pass
+        , name = "succeeds"
+        , tags = []
+        , options = []
+        , setOption = \_ _ -> Right succeeds
+        }
+    fails = TestInstance
+        { run = return $ Finished $ Fail "Always fails!"
+        , name = "fails"
+        , tags = []
+        , options = []
+        , setOption = \_ _ -> Right fails
+        }
